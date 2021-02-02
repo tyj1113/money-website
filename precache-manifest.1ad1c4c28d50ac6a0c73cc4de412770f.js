45e692116117fe4859fdb630752f8af9ccd8d3df
tyj1113 <1991383016@qq.com>
2021-02-02 19:33:08 +0800

    deploy

---
+++ precache-manifest.1ad1c4c28d50ac6a0c73cc4de412770f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7941320c266ae1371c0c",
+    "url": "/money-website/css/app.0d0ddf43.css"
+  },
+  {
+    "revision": "e64027968bb438175c5cd86be38d5382",
+    "url": "/money-website/index.html"
+  },
+  {
+    "revision": "7941320c266ae1371c0c",
+    "url": "/money-website/js/app.c1e2c097.js"
+  },
+  {
+    "revision": "792b4d5d547430e824ea",
+    "url": "/money-website/js/chunk-vendors.fb0735d7.js"
+  },
+  {
+    "revision": "6f94a71a55b472b5d6172ab127074578",
+    "url": "/money-website/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/money-website/robots.txt"
+  }
+]);
